c1e0913e3a66e96f0e4f69f87fd98c53e88c307f
c-silambu <144410442+c-silambu@users.noreply.github.com>
2026-02-03 09:00:54 +0530

    final build ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import About from "../src/pages/About"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Careers from "./pages/Careers";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <>
-    <About/>
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+    
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
